576dd9f244b93cc6dc9a65addb78b3dc74a70d58
Piyush Verma <piyush@piyushverma.net>
2018-07-21 13:21:02 +0530

    Add workspace sub commands

--- commands/cli/vars.go
+++ commands/cli/vars.go
@@ -1,9 +1,18 @@
 package main
 
-import "gopkg.in/alecthomas/kingpin.v2"
+import (
+	"log"
 
-import kingpin "gopkg.in/alecthomas/kingpin.v2"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+func varsCmd(c *kingpin.ParseContext) error {
+	log.Println("Vars commands")
+	return nil
+}
 
 func addVarsCommand(app *kingpin.Application) {
-
+	vApp := app.Command("vars", "Vars for workspace, layout and job.").Action(varsCmd)
+	vApp.Command("save", "Save the vars for the given dir tree [workspace, layout or job].")
+	vApp.Command("get", "Get vars for the given dir tree [workspace, layout or job].")
 }
--- commands/cli/watch.go
+++ commands/cli/watch.go
@@ -1,11 +1,18 @@
 package main
 
-import "gopkg.in/alecthomas/kingpin.v2"
+import (
+	"log"
 
-import kingpin "gopkg.in/alecthomas/kingpin.v2"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
 
-func addWatchCommand(app *kingpin.Application) {
-
+func watchCmd(c *kingpin.ParseContext) error {
+	log.Println("Watch commands")
+	return nil
 }
 
-
+func addWatchCommand(app *kingpin.Application) {
+	vApp := app.Command("watch", "Watch for a job.").Action(watchCmd)
+	vApp.Command("save", "Save the watch for the given job to trigger an http call on success or failure of the job.")
+	vApp.Command("get", "Get the watch for the given job ID.")
+}
