77d763875cf90d0f6ebf4c82d8f9fc1d1d0e0a52
talina06 <talina.shrotriya@gmail.com>
2018-07-21 15:02:36 +0700

    Add commands for watch and vars. GET and CREATE.

--- commands/cli/vars.go
+++ commands/cli/vars.go
@@ -13,6 +13,6 @@ func varsCmd(c *kingpin.ParseContext) error {
 
 func addVarsCommand(app *kingpin.Application) {
 	vApp := app.Command("vars", "Vars for workspace, layout and job.").Action(varsCmd)
-	vApp.Command("save", "Save the vars for the given dir tree [workspace, layout or job].")
+	vApp.Command("create", "Save the vars for the given dir tree [workspace, layout or job].")
 	vApp.Command("get", "Get vars for the given dir tree [workspace, layout or job].")
 }
--- commands/cli/watch.go
+++ commands/cli/watch.go
@@ -13,6 +13,6 @@ func watchCmd(c *kingpin.ParseContext) error {
 
 func addWatchCommand(app *kingpin.Application) {
 	vApp := app.Command("watch", "Watch for a job.").Action(watchCmd)
-	vApp.Command("save", "Save the watch for the given job to trigger an http call on success or failure of the job.")
+	vApp.Command("create", "Save the watch for the given job to trigger an http call on success or failure of the job.")
 	vApp.Command("get", "Get the watch for the given job ID.")
 }
