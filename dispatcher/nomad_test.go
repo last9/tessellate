7953b897f2d75628054fd91e5478460f06d107ec
talina06 <talina.shrotriya@gmail.com>
2018-09-10 17:10:37 +0530

    Pair coding: test case for change.

--- dispatcher/nomad_test.go
+++ dispatcher/nomad_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/tsocial/tessellate/storage/types"
+	"github.com/hashicorp/nomad/api"
 )
 
 func TestMain(m *testing.M) {
@@ -27,4 +28,24 @@ func TestMain(m *testing.M) {
 func TestClient_Dispatch(t *testing.T) {
 	job := &types.Job{Id: "job", LayoutId: "layout"}
 	assert.Nil(t, Get().Dispatch("workspace", job))
+}
+
+func TestDispatched_Job(t *testing.T) {
+	job := &types.Job{Id: "job", LayoutId: "layout"}
+	workspaceID := "workspace"
+
+	nomadClient , err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	jobID := workspaceID + "-" + job.LayoutId + "-" + job.Id
+
+	runningJob,_, err := nomadClient.Jobs().Info(jobID, nil)
+	if err!= nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, jobID, *runningJob.Name)
+
 }
