7b746927f6314fb9d99478d80efb41338b3d9946
talina06 <talina.shrotriya@gmail.com>
2018-09-14 18:20:39 +0530

    Change err var names

--- commands/commons/commons_test.go
+++ commands/commons/commons_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 func TestCandidateFiles(t *testing.T) {
-	t.Run("Check default list of files", func(t *testing.T) {
+	t.Run("Check default list of files, no manifest", func(t *testing.T) {
 		f, err := CandidateFiles("testdata", nil)
 		if err != nil {
 			t.Fatal(err)
@@ -22,7 +22,7 @@ func TestCandidateFiles(t *testing.T) {
 		assert.Equal(t, f, []string{"testdata/hello.tf.json"})
 	})
 
-	t.Run("Check custom list of files", func(t *testing.T) {
+	t.Run("Check custom list of files, as per the manifest data", func(t *testing.T) {
 		f, err := CandidateFiles("testdata", []string{".txt"})
 		if err != nil {
 			t.Fatal(err)
@@ -31,17 +31,19 @@ func TestCandidateFiles(t *testing.T) {
 		assert.Equal(t, f, []string{"testdata/a.txt", "testdata/hello.tf.json"})
 	})
 
-	t.Run("Should give both tmpl and tf.json files as per manifest", func(t *testing.T) {
+	t.Run("Should return both .tmpl and tf.json files as per manifest", func(t *testing.T) {
 		f, err := CandidateFiles("testdata", []string{".tmpl"})
 		if err != nil {
 			t.Fatal(err)
 		}
-		fmt.Println(f)
+		if len(f) < 2 {
+			t.Fatal(fmt.Sprintf("Expected 2 extensions, .tmpl and .tf.json, got %v", f))
+		}
 	})
 }
 
 func TestReadFileLines(t *testing.T) {
-	t.Run("Should return manifest file contents and return extensions an array of string.", func(t *testing.T) {
+	t.Run("Should return manifest file contents and return extensions as an array of string.", func(t *testing.T) {
 		expected := []string{".txt\n", ".tmpl"}
 		lines, err := ReadFileLines("testdata/.tsl8")
 		if err != nil {
@@ -76,25 +78,25 @@ func TestReadFileLines(t *testing.T) {
 
 func TestFileType(t *testing.T) {
 	t.Run("Should return actual file extensions and not txt", func(t *testing.T) {
-		fileType, err := GetFileContentType("testdata/presentation.txt")
-		if err != nil {
-			t.Fatal(err)
+		fileType, err1 := GetFileContentType("testdata/presentation.txt")
+		if err1 != nil {
+			t.Fatal(err1)
 		}
 		if fileType != "zip" {
 			t.Fatal(fmt.Sprintf("Expected to be a zip file, got %v", fileType))
 		}
 
-		fileType2, err := GetFileContentType("testdata/movie.txt")
-		if err != nil {
-			t.Fatal(err)
+		fileType2, err2 := GetFileContentType("testdata/movie.txt")
+		if err2 != nil {
+			t.Fatal(err2)
 		}
 		if fileType2 != "mp4" {
 			t.Fatal(fmt.Sprintf("Expected to be a mp4 file, got %v", fileType2))
 		}
 
-		fileType3, err := GetFileContentType("testdata/main.txt")
-		if err != nil {
-			t.Fatal(err)
+		fileType3, err3 := GetFileContentType("testdata/main.txt")
+		if err3 != nil {
+			t.Fatal(err3)
 		}
 		if fileType3 != "elf" {
 			t.Fatal(fmt.Sprintf("Expected to be a mp4 file, got %v", fileType3))
@@ -102,30 +104,29 @@ func TestFileType(t *testing.T) {
 	})
 
 	t.Run("Should identify all text files irrespective of extensions.", func(t *testing.T) {
-		fileType, err := GetFileContentType("testdata/a.txt")
-		if err != nil {
-			t.Fatal(err)
+		fileType, err1 := GetFileContentType("testdata/a.txt")
+		if err1 != nil {
+			t.Fatal(err1)
 		}
 		if fileType != "unknown" {
 			t.Fatal(fmt.Sprintf("Expected to be a text file, got %v", fileType))
 		}
 
-		fileType2, err := GetFileContentType("testdata/newline.tsl8")
-		if err != nil {
-			t.Fatal(err)
+		fileType2, err2 := GetFileContentType("testdata/newline.tsl8")
+		if err2 != nil {
+			t.Fatal(err2)
 		}
 		if fileType2 != "unknown" {
 			t.Fatal(fmt.Sprintf("Expected to be a text file, got %v", fileType2))
 		}
 
-		fileType3, err := GetFileContentType("testdata/hello.tf.json")
-		if err != nil {
-			t.Fatal(err)
+		fileType3, err3 := GetFileContentType("testdata/hello.tf.json")
+		if err3 != nil {
+			t.Fatal(err3)
 		}
 		if fileType3 != "unknown" {
 			t.Fatal(fmt.Sprintf("Expected to be a text file, got %v", fileType3))
 		}
-
 	})
 }
 
